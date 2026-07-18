108f2e24e0b1c7c063dca456271333de1c4f4e7e
Radosław Bogusławski <boguslawskiradek@gmail.com>
2026-07-18 08:26:47 +0200

    Added cube primitive

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -6,7 +6,7 @@
 Renderer::Renderer(Shader* shader) 
     : shader{shader} {
     glClearColor(1.0f, 0.0f, 1.0f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glEnable(GL_DEPTH_TEST);
 
     glGenVertexArrays(1, &vao);
     glGenBuffers(1, &vbo);
@@ -15,10 +15,10 @@ Renderer::Renderer(Shader* shader)
     glBindVertexArray(vao);
 
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(TRIANGLE_VERTICES), TRIANGLE_VERTICES, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(CUBE_VERTICES), CUBE_VERTICES, GL_STATIC_DRAW);
 
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(TRIANGLE_INDICES), TRIANGLE_INDICES, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(CUBE_INDICES), CUBE_INDICES, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), 0);
     glEnableVertexAttribArray(0);
@@ -30,7 +30,7 @@ Renderer::Renderer(Shader* shader)
     glUseProgram(shader->program);
 
     glm::mat4 model = glm::mat4(1.0f);
-    model = glm::rotate(model, glm::pi<float>() * 0.25f, glm::vec3(1.0f, 1.0f, 0.0f));
+    model = glm::rotate(model, glm::pi<float>() * 0.5f, glm::vec3(1.0f, 1.0f, 0.0f));
     shader->SetUniformMatrix4fv("model", model);
 }
 
@@ -46,5 +46,5 @@ void Renderer::Draw() {
     glUseProgram(shader->program);
     glBindVertexArray(vao);
     // glDrawArrays(GL_TRIANGLES, 0, 3);
-    glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+    glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
 }
